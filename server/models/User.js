853cfdb24745514ca51f33ad488f7043747a0218
michael seaman <mseaman26@gmail.com>
2023-02-17 22:37:42 -0800

    password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -42,3 +42,16 @@ const userSchema = new Schema ({
     },
 
 })
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+const User = model('User', userSchema)
+
+module.exports = User
